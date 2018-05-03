3aca9a39b16ab97af73e1758a3be341527c2b953
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-03 11:53:28 +1000

    fix node class initialize method

--- node.rb
+++ node.rb
@@ -1,10 +1,9 @@
 class Node
   def initialize(value, child_1, child_2, parent)
+    @value = value
+    @child_1 = child_1
+    @child_2 = child_2
+    @parent = parent
   end
 
-  @value = value
-  @child_1 = child_1
-  @child_2 = child_2
-  @parent = parent
-  
 end
